f1b5ebb592eed2447e3c1c97cdaf588ee0b237ef
Nicholas Thiago Xavier Costa <nicholasxavis23@gmail.com>
2023-12-03 15:57:11 -0700

    final commit 4

--- my-app/app/api/register/route.js
+++ my-app/app/api/register/route.js
@@ -1,6 +1,6 @@
 
 import { connectMongoDB } from "@/lib/mongodb";
-import User from "@/models/user";
+import {User} from "@/models/user";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
