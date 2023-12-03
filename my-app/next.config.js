c03dacb90b179e30889abc7b8ac869a370a9ed33
Nicholas Thiago Xavier Costa <nicholasxavis23@gmail.com>
2023-12-03 15:44:55 -0700

    final commit 2

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		'NEXTAUTH_SECRET': '=8oWaqhRc7IUSbo0wQrb4hX33twx7veoBSIEFnq8kzQQ=',
+		'MONGODB_URI': '=mongodb+srv://dbuser:cprg306@cprg-306-a3-main.t0cwujd.mongodb.net/?retryWrites=true&w=majority',
+	}
+}
 
 module.exports = nextConfig
