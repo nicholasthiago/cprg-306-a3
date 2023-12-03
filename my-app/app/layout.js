5ea5f85b93bf9e08aa372baa223e579acbf11cc1
Nicholas Thiago Xavier Costa <nicholasxavis23@gmail.com>
2023-12-03 15:26:04 -0700

    final commit 2

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -2,8 +2,8 @@
 import { AuthProvider } from './Providers';
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
